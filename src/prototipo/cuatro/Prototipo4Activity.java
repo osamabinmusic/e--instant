34d49b6dfd50b2d1f4dc19e803f044f60ebd5478
vitoco <osamabinmusic@hotmail.com>
2012-06-28 15:11:01 -0400

    first!!

---
+++ src/prototipo/cuatro/Prototipo4Activity.java
@@ -0,0 +1,13 @@
+package prototipo.cuatro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Prototipo4Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
