3b9ac7707e4225ac99f453d2660c0ac548fc0f3d
vitoco <osamabinmusic@hotmail.com>
2012-06-28 21:01:39 -0400

    madafaka!!

--- gen/prototipo/cuatro/R.java
+++ gen/prototipo/cuatro/R.java
@@ -11,7 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int back1=0x7f020000;
+        public static final int bombe=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int paco=0x7f020003;
+        public static final int pdi=0x7f020004;
+        public static final int samu=0x7f020005;
     }
     public static final class id {
         public static final int btnAmbu=0x7f050002;
@@ -26,5 +31,6 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int palcopiright=0x7f040002;
     }
 }
