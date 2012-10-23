a3866deb258eeb879e8d67297d58d7c981382fc0
vitoco <osamabinmusic@hotmail.com>
2012-10-22 23:01:39 -0300

    Se agregan funciones de GPS ... Versión 2.0.1

--- src/prototipo/cuatro/Prototipo4Activity.java
+++ src/prototipo/cuatro/Prototipo4Activity.java
@@ -7,6 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
 
 public class Prototipo4Activity extends Activity {
     /** Called when the activity is first created. */
@@ -51,6 +57,40 @@ public class Prototipo4Activity extends Activity {
         	    startActivity(i);
         	}
     	});
-        
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+
+        LocationManager milocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener milocListener = new MiLocationListener();
+
+        milocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, milocListener);
+    }
+    
+    public class MiLocationListener implements LocationListener{
+
+    	public void onLocationChanged(Location loc){
+    		loc.getLatitude();
+    		loc.getLongitude();
+    		
+    		String coordenadas = "Mis coordenadas son: "
+    				+ "Latitud = " + loc.getLatitude()
+    				+ "Longitud = " + loc.getLongitude();
+    		
+    		Toast.makeText( getApplicationContext(),coordenadas,Toast.LENGTH_LONG).show();
+    	}
+    
+    	public void onProviderDisabled(String provider){
+    		Toast.makeText( getApplicationContext(),"Gps Desactivado",Toast.LENGTH_SHORT ).show();
+    	}
+    	
+    	public void onProviderEnabled(String provider){
+    		Toast.makeText( getApplicationContext(),"Gps Activo",Toast.LENGTH_SHORT ).show();
+    	}
+    	
+    	public void onStatusChanged(String provider, int status, Bundle extras){}
+    	
     }
 }
